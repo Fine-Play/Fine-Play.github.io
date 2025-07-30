8b53779490bc52b77e3f47b2492dedea37fdb51e
ByunSeungJun <bsj0106@gachon.ac.kr>
2025-07-31 04:18:57 +0900

    0731_0418

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2214078523"
+    serviceWorkerVersion: "3166442688"
   }
 });
